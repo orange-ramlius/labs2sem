f54a3c104521c901dfd87bd01b53269cd5171efc
bincell <vasram2706@gmail.com>
2024-05-28 17:08:37 +0400

    removed binaries, added gitignore

--- 1/1.1/main.cpp
+++ 1/1.1/main.cpp
@@ -2,41 +2,32 @@
 using namespace std;
 
 class A {
-    int a, b;
+  int a, b;
+
 public:
-    A (A & x) {
-        a = x.a;
-        b = x.b;
-        cout << 1;
-    }
-
-    A () {
-        a = 0;
-        b = 0;
-    }
-
-    A (int a) {
-        this -> a = a;
-        b = a;
-        cout << 2;
-    } 
-
-    A (double a, int b) {
-        this -> a = (int)a;
-        this -> b = b;
-    }
+  A(A &x) {
+    a = x.a;
+    b = x.b;
+    cout << 1;
+  }
+  A(int a = 0, int b = 0) {
+    this->a = a;
+    b = a;
+    cout << 2;
+  }
 };
-
-void f () {
-    A x (1);
-    A y;
-    A z = A (2.5, 4);
-    A s = 6;
-    A w = z;
-    x = z = w;
+void f() {
+  A x(1);
+  A y;
+  A z = A(2.5, 4);
+  A s = 6;
+  A w = z;
+  x = z = w;
 }
 
 int main(void) {
-    f();
-    return 0;
+  f();
+  return 0;
 }
+
+//при компиляции при помощи gcc с оптимизацией и без нее вывод 22221
